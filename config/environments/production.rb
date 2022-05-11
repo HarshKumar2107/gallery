b7998f792f1efd5b1b30bee140e21520f3822eda
Harsh Kumar <harshmhra@gmail.com>
2022-05-11 11:28:36 +0530

    add code to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+config.action_mailer.default_url_options = { :host => "https://gallerybb.herokuapp.com/" }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :user_name => '1042ad4ca262ae',
+  :password => '601b21663763dc',
+  :address => 'smtp.mailtrap.io',
+  :domain => 'smtp.mailtrap.io',
+  :port => '2525',
+  :authentication => :cram_md5
+}
 end
